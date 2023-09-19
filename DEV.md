570aaeb8abee2650e0f1645c746d82eed90369dd
danellecline <dcline@mbari.org>
2023-09-19 16:38:20 -0700

    perf: pydantic flow and added missing deps

--- DEV.md
+++ DEV.md
@@ -5,7 +5,7 @@ Install for local development with
 ## Follow the README.md instructions for installing the conda environment
 
 The tests require the example data to be downloaded and a working web server that
-can service video with test video. This can be done with the following command:
+can service video with test video. This can be done with the following:
 
 ```shell
 ./bin/start.sh
@@ -19,10 +19,8 @@ export AWS_DEFAULT_PROFILE=minio-accutrack
 cd src/app && uvicorn main:app --reload
 ```
  
-This will start the server on port 8000. 
-
-```shell  
-
+This will start the server on port 8000. Open the browser to http://localhost:8000/docs to see the API documentation.
+ 
 ## Testing
 
 Run the pytest tests from the root directory with:
--- src/app/qmanager/monitor.py
+++ src/app/qmanager/monitor.py
@@ -15,7 +15,7 @@ from deepsea_ai.database.job.database_helper import get_status, update_media
 from sqlalchemy.orm import sessionmaker
 
 from app.conf import local_config_ini_path
-from app.job import Job2
+from app.job import Job2, PydanticJobWithMedia2
 from app.logger import debug, info, err
 from app.runner.local import DockerRunner, default_name
 
@@ -58,74 +58,72 @@ class Monitor:
 
         info(f'Found {len(all_containers)} active {default_name} docker containers')
 
+        job_data = None
+        job = None
         with self.session_maker.begin() as db:
             docker_jobs = db.query(Job2).filter(Job2.job_type == JobType.DOCKER).all()
-            info(f'Found {len(docker_jobs)} docker jobs')
             # Get all the job ids with status QUEUED and RUNNING
             jobs_ids_queued = [job.id for job in docker_jobs if get_status(job) == Status.QUEUED]
             jobs_ids_running = [job.id for job in docker_jobs if get_status(job) == Status.RUNNING]
+            info(f'Found {len(docker_jobs)} docker jobs in the database. '
+                 f'Number of queued jobs: {len(jobs_ids_queued)}. Number of running jobs: {len(jobs_ids_running)}')
 
-        if len(jobs_ids_queued) > 0 and len(all_containers) == 0:
-            with self.session_maker.begin() as db:
+            if len(jobs_ids_queued) > 0 and len(all_containers) == 0:
                 # Get the first job in the queue
                 job = db.query(Job2).filter(Job2.id == jobs_ids_queued[0]).first()
+                job_data = PydanticJobWithMedia2.from_orm(job)
+                update_media(db, job, job.media[0].name, Status.RUNNING)
 
-                video_url = job.media[0].name  # Docker jobs have 1 media per job, so get the first one
-
-                # Make a prefix for the output based on the video path (sans http) and the current time
-                video_s3 = Path(urlparse(video_url).path)
-                key = f"{video_s3.parent}{datetime.utcnow().strftime('%Y%m%dT%H%M%SZ')}"
-                output_s3 = f"s3://{self.s3_root_bucket}/{self.s3_track_prefix}/{key}/output"
-
-                update_media(db, job, video_url, Status.RUNNING)
-                model_s3 = job.engine
-                job_id = job.id
-
-            # Create a docker runner instance and run it
-            info(f'Running job {job_id} with output {output_s3}')
-            instance = DockerRunner(job_id=job_id,
+        if job_data:
+            # Make a prefix for the output based on the video path (sans http) and the current time
+            video_s3 = Path(urlparse(job_data.media[0].name).path)
+            key = f"{video_s3.parent}{datetime.utcnow().strftime('%Y%m%dT%H%M%SZ')}"
+            output_s3 = f"s3://{self.s3_root_bucket}/{self.s3_track_prefix}/{key}/output"
+            info(f'Running job {job_data.id} with output {output_s3}')
+            instance = DockerRunner(job_id=job_data.id,
                                     output_s3=output_s3,
-                                    video_url=video_url,
-                                    model_s3=model_s3,
+                                    video_url=job_data.media[0].name,
+                                    model_s3=job_data.engine,
                                     track_s3=self.s3_strongsort_track_config,
                                     args='--iou-thres 0.5 --conf-thres 0.01 --agnostic-nms --max-det 100')
 
             instance.run()
 
             with self.session_maker.begin() as db:
-                job = db.query(Job2).filter(Job2.id == jobs_ids_queued[0]).first()
+                job = db.query(Job2).filter(Job2.id == job_data.id).first()
                 job.engine = instance.container.id
-                update_media(db, job, video_url, Status.RUNNING)
+                update_media(db, job, job.media[0].name, Status.RUNNING)
                 self.notify(job, Status.RUNNING)
 
-            # Wait for the container to finish
+            # Wait for the container to finish or timeout after 3 tries
             num_tries = 0
             while not instance.is_successful() and num_tries < 3:
                 time.sleep(30)
                 num_tries += 1
 
             with self.session_maker.begin() as db:
+                job = db.query(Job2).filter(Job2.id == job_data.id).first()
                 if instance.is_successful():
                     info(f'Processing complete: {instance.is_successful()}')
-                    update_media(db, job, video_url, Status.SUCCESS)
                     self.notify(job.email, Status.SUCCESS)
+                    update_media(db, job, job.media[0].name, Status.SUCCESS)
                 else:
                     err(f'Processing complete: {instance.is_successful()}')
-                    update_media(db, job, video_url, Status.FAILED)
+                    update_media(db, job, job.media[0].name, Status.FAILED)
                     self.notify(job.email, Status.FAILED)
 
-            info(f'Job {job.id} finished running')
+            info(f'Job {job_data.id} finished running')
 
         if len(jobs_ids_running) > 0:
             for job_id in jobs_ids_running:
-                # Should never get here unless something went wrong and the service was restarted
+                # Should never get here unless something went wrong and the
+                # service was restarted while a job was running
                 err(f'Job {job_id} was running but the service was restarted')
                 with self.session_maker.begin() as db:
                     job = db.query(Job2).filter(Job2.id == job_id).first()
                     job.media[0].status = Status.FAILED
                     # Stop the container by its id and remove it
-                    container_id = job.engine
-                    container = client.containers.get(container_id)
+                    container = client.containers.get(job.engine)
                     if container:
                         container.stop()
                         container.remove()
