c90cbadf6274c8327922a79d8743f6b9d252a50b
danellecline <dcline@mbari.org>
2023-09-29 12:52:08 -0700

    chore: minor clean-up from test

--- bin/run_dev.sh
+++ bin/run_dev.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+# Build the stack for development
+# Fetch a few videos to serve in the default nginx/video directory and start the stack
+# Run with ./run_dev.sh
 
 # Get the directory of this script and the base directory
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
@@ -6,7 +9,7 @@ BASE_DIR="$(cd "$(dirname "${SCRIPT_DIR}/../.." )" && pwd )"
 
 cd $BASE_DIR
 echo "Build the docker images"
-#bin/build_docker.sh
+./bin/build_docker.sh
 
 # Get the short version of the hash of the commit
 git_hash=$(git log -1 --format=%h)
@@ -23,23 +26,20 @@ export $(grep -v '^#' $BASE_DIR/.env.dev |  xargs)
 DATA_DIR=${DATA_DIR/\$\{HOME\}/$HOME}
 
 echo "Fetch a few videos to serve in the default nginx/video directory"
-mkdir -p $HOME/fastapi_localtrack_dev/nginx/video
-pushd $HOME/fastapi_localtrack_dev/nginx/video
+mkdir -p ${DATA_DIR}/nginx/video
 # Fetch each video if it doesn't already exist
 declare -a video_urls=(
   "https://902005-public.s3.us-west-2.amazonaws.com/video/10frame/V4361_20211006T162656Z_h265_10frame.mp4"
   "https://902005-public.s3.us-west-2.amazonaws.com/video/1sec/V4361_20211006T162656Z_h265_1sec.mp4"
   "https://902005-public.s3.us-west-2.amazonaws.com/video/1min/V4361_20211006T162656Z_h265_1min.mp4"
 )
-mkdir -p ${DATA_DIR}/nginx/video
 for url in "${video_urls[@]}"
 do
   file_name=$(basename $url)
   if [ ! -f ${DATA_DIR}/nginx/video/$file_name ]; then
-  curl -O $url -z ${DATA_DIR}/nginx/video/$file_name
+  curl -O $url ${DATA_DIR}/nginx/video/$file_name
   fi
 done
-popd
 
 echo "Nginx server running at http://localhost:8090"
 echo "Minio server running at http://localhost:7000"
@@ -47,7 +47,8 @@ echo "FastAPI server running at http://localhost:8001"
 echo "FastAPI docs running at http://localhost:8001/docs"
 
 ### Run the daemon and the api server
-#conda activate fastapi-localtrack
+conda activate fastapi-localtrack
+# this overrides the value in .env.dev which refers to the internal docker name
 export MINIO_ENDPOINT_URL=http://localhost:7000
 export PYTHONPATH=$BASE_DIR/src:$BASE_DIR/tests
 cd $BASE_DIR/src/app
