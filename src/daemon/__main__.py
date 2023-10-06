dcaf16779ddb5c5c766edc5ccde3ddea3ad7ad2b
danellecline <dcline@mbari.org>
2023-10-06 13:25:06 -0700

    perf: removed blocking wait and stderr/out logs which need to be moved to the container; move docker pull to main scope - should fail if docker images cannot be found

--- src/daemon/__main__.py
+++ src/daemon/__main__.py
@@ -4,9 +4,10 @@
 
 from dependency_injector.wiring import Provide, inject
 
+from aiodocker import Docker, DockerError
 from daemon.dispatcher import Dispatcher
 from daemon.container import Container
-from daemon.logger import create_logger_file, info
+from daemon.logger import create_logger_file, info, err
 import asyncio
 import yaml
 import os
@@ -16,6 +17,7 @@ from daemon.misc import verify_upload
 # Setup logging
 log_path = Path(os.path.dirname(__file__)).parent.parent / 'logs'
 create_logger_file(log_path, 'daemon')
+
 
 @inject
 def main(dispatcher: Dispatcher = Provide[Container.dispatcher]) -> None:
@@ -49,6 +51,24 @@ def env_check() -> bool:
     return True
 
 
+async def docker_check(data: dict) -> bool:
+    # if running this on an arm64 machine, use the arm64 docker image
+    if os.uname().machine == 'arm64':
+        image_name = data['monitors']['docker']['strongsort_container_arm64']
+    else:
+        image_name = data['monitors']['docker']['strongsort_container']
+    async with Docker() as docker_aoi:
+        try:
+            return await docker_aoi.images.inspect(image_name)
+        except DockerError as e:
+            if e.status == 404:
+                await docker_aoi.pull(image_name)
+            else:
+                raise DockerError(e.status, f'Error retrieving {image_name} image.')
+
+        return False
+
+
 async def run():
     yaml_path = Path(os.path.dirname(__file__)).parent.parent / 'config.yml'
     if not yaml_path.exists():
@@ -62,10 +82,14 @@ async def run():
             return False
 
         check_minio = await minio_check(data)
+        check_docker = await docker_check(data)
 
-        # Exit if we can't upload to minio
+        # Exit if we can't upload to minio or pull docker images
         if not check_minio:
-            print(f"Could not upload to minio")
+            err(f"Could not upload to minio")
+            return False
+        if not check_docker:
+            err(f"Could not pull docker image")
             return False
 
         # Initialize the container
